488136337138bfd8434619bc81b1e5e8d8ef7f7c
임윤희 <lyhhh0320@khu.ac.kr>
2021-11-19 00:48:36 +0900

    Add signup

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 //사용자 스키마
 var UserSchema = new mongoose.Schema({
-    name: {type: String, required: false}, //false인 이유: 네이버 로그인 시 사용자 이름을 가져오지 못하는 issue 때문
+    name: {type: String, required: false},
     id: {type: String, required: true, unique: true},
     pw: {type: String, required: false} //false인 이유: 소셜로그인시 비밀번호 필요없음
 });
--- routes/video.js
+++ routes/video.js
@@ -3,31 +3,33 @@ var router = express.Router();
 var model = require('../models');
 var Videos = model.Videos;
 
-//(미완성)
-router.post('/search', function(req, res){
-    var keyWord = req.body.keyWord;
-    Videos.findOne({$or: [ //동영상 제목 또는 내용, 출연진으로 검색
-        {title: keyWord},
-        {content: keyWord},
-        {cast: keyWord}
-        ]},(err, video)=>{
-        if (err) res.status(500).send('검색 에러');
-		else if (video){ //관련 동영상이 있을 경우
-            //정확히 찾는 동영상이 있을 경우
-            if(video.title == keyWord) {
-                res.redirect('/result');
-            }
-            //찾는 동영상은 없지만 내용 또는 출연진이 일치하는 다른 동영상이 있을 경우
-            else if(video.content ==keyWord || video.cast == keyWord){}
-        } 
-		else { //관련 동영상이 없을 경우
-            res.redirect('/search');
-        }
-    });
-});
-
-router.get('/result', function(req, res){
-   // res.render('/video/result_page');
+router.get('/search', function(req, res){
+    res.render('video/search_page',{result:null});
 })
+
+router.post('/search', function(req, res){
+    var keyword = req.body.keyword;
+    console.log('keyword:' + keyword);
+    if(keyword =='') res.render('video/search_page', {result:false});
+    else{
+    Videos.findOne({title:keyword}, (err, video)=>{
+        if (err) res.status(500).send('검색 에러');
+        else if (video){ //정확히 찾는 동영상이 있을 경우
+            console.log(video.cast);
+            res.send(video);
+            //res.render('video/search_page', {result:true});
+        }
+        else if(!video){ //제목으로 나오지 않을 경우 내용과 출연진으로 검색
+            Videos.find({$or: [{content: {$regex: keyword, $options: 'i'}}, {cast: {$regex: keyword, $options: 'i'}}]}, (err, videos)=>{
+                if(videos.length != 0){
+                    res.send(videos);
+                }else { //내용과 출연진 검색으로도 나오지 않을 경우
+                    result = false;
+                    res.render('video/search_page', {result:false});
+                }
+            });
+        }
+    });}
+});
 
 module.exports = router;
