9054f3465f228ff2a590ad57a376629153f325b6
aylee <miiia@khu.ac.kr>
2021-12-06 18:06:01 +0900

    Fix menu, login and leave out modal and add details
    Former-commit-id: cca20aca963ffd8790ba6c5b3c3f14a75d30551e [formerly 1b06d4e9489063d6f26ac02566b826a6ffcd2cd6] [formerly 8c7d4c4560c0ecc8ba9a459838323d8f4673f83c [formerly 6ff406ca631f402ab38ae7fb8486e635cba03400]]
    Former-commit-id: 4b2e3f01ae6f78d5586554b488136bee940d3ca0 [formerly 65effba82ff059dadea83469e463143435f4952d]
    Former-commit-id: 198b12d46a253663b94d8ab6599ca8f4f53e1581

--- config/secret.js
+++ config/secret.js
@@ -1,40 +1,21 @@
-//  module.exports={
-//      'mongoDB':{
-//          'password':'******'
-//      },
-//      'naver':{
-//          'clientID':'******',
-//          'clientSecret':'******',
-//          'callbackURL':'******'
-//      },
-//      'kakao':{
-//          'clientID': '******',
-//          'callbackURL': '******'
-//      },
-//      'facebook':{
-//          'clientID': '******',
-//          'clientSecret': '******',
-//          'callbackURL':'******'
-//      }
-//  }
-
-
-module.exports={
-    'mongoDB':{
-        'password':'admin1234'
-    },
-    'naver':{
-        'clientID':'lyMvEB4gnJ9frBgLPy_C',
-        'clientSecret':'ZPYJJrIZRO',
-        'callbackURL':'http://localhost:4000/auth/naver/callback'
-    },
-    'kakao':{
-        'clientID': '384645c169990c67fb63e2f99d794ad6',
-        'callbackURL': 'http://localhost:4000/auth/kakao/callback'
-    },
-    'facebook':{
-        'clientID': '261415312610713',
-        'clientSecret': '932f63728e1bad9b21db7a89b8dfa18f',
-        'callbackURL':'http://localhost:4000/auth/facebook/callback'
-    }
+  module.exports={
+     'mongoDB':{
+         'password':'******'
+      },      
+      'naver':{
+          'clientID':'******',
+          'clientSecret':'******',         
+          'callbackURL':'******'
+     },
+      'kakao':{
+         'clientID': '******',
+         'callbackURL': '******'
+     },
+     'facebook':{
+         'clientID': '******',
+         'clientSecret': '******',
+        'callbackURL':'******'
+      }
 }
+
+
