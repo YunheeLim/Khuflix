58c5c177d0f54a4299cd0dc03a1aaf2bdf62de93
Sun <icecream20@khu.ac.kr>
2021-12-06 11:50:24 +0900

    Fix details in first, login, fail, signup page

--- config/secret.js
+++ config/secret.js
@@ -1,19 +1,39 @@
+// module.exports={
+//     'mongoDB':{
+//         'password':'******'
+//     },
+//     'naver':{
+//         'clientID':'******',
+//         'clientSecret':'******',
+//         'callbackURL':'******'
+//     },
+//     'kakao':{
+//         'clientID': '******',
+//         'callbackURL': '******'
+//     },
+//     'facebook':{
+//         'clientID': '******',
+//         'clientSecret': '******',
+//         'callbackURL':'******'
+//     }
+// }
+
 module.exports={
     'mongoDB':{
-        'password':'******'
+        'password':'admin1234'
     },
     'naver':{
-        'clientID':'******',
-        'clientSecret':'******',
-        'callbackURL':'******'
+        'clientID':'lyMvEB4gnJ9frBgLPy_C',
+        'clientSecret':'ZPYJJrIZRO',
+        'callbackURL':'http://localhost:4000/auth/naver/callback'
     },
     'kakao':{
-        'clientID': '******',
-        'callbackURL': '******'
+        'clientID': '384645c169990c67fb63e2f99d794ad6',
+        'callbackURL': 'http://localhost:4000/auth/kakao/callback'
     },
     'facebook':{
-        'clientID': '******',
-        'clientSecret': '******',
-        'callbackURL':'******'
+        'clientID': '261415312610713',
+        'clientSecret': '932f63728e1bad9b21db7a89b8dfa18f',
+        'callbackURL':'http://localhost:4000/auth/facebook/callback'
     }
 }
