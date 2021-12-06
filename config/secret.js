33f9256645ffe327d9270f7d791ac283fb876318
aylee <miiia@khu.ac.kr>
2021-12-06 22:32:53 +0900

    Fix bugs on slider
    Former-commit-id: 8a217d06c0bbf06c42c6c387c000de9233a69021 [formerly 7a9d4f1554db39b92699f9ffa1592cad31f35872] [formerly c323470f52b980f4c3c27b6be65e0c8fb0ba74ee [formerly 8d5a7503b74a3e89f22e8ccf52220afb8bccc0de]]
    Former-commit-id: 0024b14025efe4730d10175ee718d9106200c66a [formerly d6d154024bfa2b373ff90751d12aa62fc44b42c9]
    Former-commit-id: 285033b38732e8d0e2b85d4273e8c1f16f44a4ae

--- config/secret.js
+++ config/secret.js
@@ -1,21 +1,19 @@
-  module.exports={
-     'mongoDB':{
-         'password':'******'
-      },      
-      'naver':{
-          'clientID':'******',
-          'clientSecret':'******',         
-          'callbackURL':'******'
-     },
-      'kakao':{
-         'clientID': '******',
-         'callbackURL': '******'
-     },
-     'facebook':{
-         'clientID': '******',
-         'clientSecret': '******',
-        'callbackURL':'******'
-      }
+module.exports={
+  'mongoDB':{
+      'password':'******'
+  },
+  'naver':{
+      'clientID':'******',
+      'clientSecret':'******',
+      'callbackURL':'******'
+  },
+  'kakao':{
+      'clientID': '******',
+      'callbackURL': '******'
+  },
+  'facebook':{
+      'clientID': '******',
+      'clientSecret': '******',
+      'callbackURL':'******'
+  }
 }
-
-
