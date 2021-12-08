12195d3368e0cd5ce89ac8dee5600a35c915dd48
Sun <icecream20@khu.ac.kr>
2021-12-09 01:12:45 +0900

    Fix search page, detail page, etc.
    Former-commit-id: 153c6c3632fdad8f7e568032d7fa86f71baae47e [formerly 7146eb5f708ced41e818820d7b22eebb348a310e] [formerly 4e62b5e19b0fd6454c506c3b4715ccd2a244d011 [formerly a4a7a5c9db6579706642c5852f450028688c99c8]]
    Former-commit-id: f2e5345e821fe5fba46482b79d5b0b40aa72303a [formerly 356ca4e5c9c5c0baf45e64fbb8766db42d2c7a7b]
    Former-commit-id: 06f2c692520ef8087276ff41ec2985f5a37e6d38

--- config/secret.js
+++ config/secret.js
@@ -1,19 +1,20 @@
 module.exports={
     'mongoDB':{
-        'password':''
+        'password':'admin1234',
+        'local_URL':'mongodb://localhost:27017/yourDBname'
     },
     'naver':{
-        'clientID':'',
-        'clientSecret':'',
-        'callbackURL':'http://khuflix.osschatbot.ga:4000/auth/naver/callback'
+        'clientID':'lyMvEB4gnJ9frBgLPy_C',
+        'clientSecret':'ZPYJJrIZRO',
+        'callbackURL':'http://localhost:4000/auth/naver/callback'
     },
     'kakao':{
-        'clientID': '',
-        'callbackURL': 'http://khuflix.osschatbot.ga:4000/auth/kakao/callback'
+        'clientID': '384645c169990c67fb63e2f99d794ad6',
+        'callbackURL': 'http://localhost:4000/auth/kakao/callback'
     },
-    //'facebook':{
-      //  'clientID': '261415312610713',
-        //'clientSecret': '932f63728e1bad9b21db7a89b8dfa18f',
-        //'callbackURL':'http://khuflix.osschatbot.ga:4000/auth/facebook/callback'
-   // }
+    'facebook':{
+        'clientID': '261415312610713',
+        'clientSecret': '932f63728e1bad9b21db7a89b8dfa18f',
+        'callbackURL':'http://localhost:4000/auth/facebook/callback'
+    }
 }
