2a6705f90edf4d9c7d41016d0b60968ac11ac5d6
이아영 <miiia@khu.ac.kr>
2021-12-08 14:12:18 +0000

    Add photos and vid
    Former-commit-id: 9b478af172eebac2ac1ac1131c23e5f60467860c [formerly a9bb6e4cce7c775b1f58a47f07c2cb5e694c9d29] [formerly b1f0a42a8890ca9582e2fd7bf49f12b3dd7a6a19 [formerly 195d112bfad866497ddbac5cc42b28b9a59cbc98]]
    Former-commit-id: 9cea5fb7b2d95d26f9b1397f261d3444254dab60 [formerly 4b09e8552a84a98cd327d68cc686b6296e8b757a]
    Former-commit-id: 6d25c76b2e3f454552986f659c40a701ae4f069b

--- config/secret.js
+++ config/secret.js
@@ -1,19 +1,19 @@
 module.exports={
     'mongoDB':{
-        'password':''
+        'password':'admin1234'
     },
     'naver':{
-        'clientID':'',
-        'clientSecret':'',
-        'callbackURL':'http://khuflix.osschatbot.ga:4000/auth/naver/callback'
+        'clientID':'lyMvEB4gnJ9frBgLPy_C',
+        'clientSecret':'ZPYJJrIZRO',
+        'callbackURL':'http://localhost:4000/auth/naver/callback'
     },
     'kakao':{
-        'clientID': '',
-        'callbackURL': 'http://khuflix.osschatbot.ga:4000/auth/kakao/callback'
+        'clientID': '384645c169990c67fb63e2f99d794ad6',
+        'callbackURL': 'http://localhost:4000/auth/kakao/callback'
     },
-    //'facebook':{
-      //  'clientID': '261415312610713',
-        //'clientSecret': '932f63728e1bad9b21db7a89b8dfa18f',
-        //'callbackURL':'http://khuflix.osschatbot.ga:4000/auth/facebook/callback'
-   // }
+    'facebook':{
+        'clientID': '336453607904096',
+        'clientSecret': '932f63728e1bad9b21db7a89b8dfa18f',
+        'callbackURL':'http://localhost:4000/auth/facebook/callback'
+    }
 }
